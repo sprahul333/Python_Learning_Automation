e416beca97a3b190b5627302f66d48fdb1839e25
sprahul333 <sprahul333@gmail.com>
2024-06-04 20:03:35 +0530

    Intial Push of the Code for Selenium Automation

---
+++ samplePythonCodes/Lab001.py
@@ -0,0 +1,10 @@
+#This is called as single line comment
+print("Hello World"); #Prints the data onto the console
+print("This is my first program")
+print("This is my first program")
+print("This is my first program")
+print("This is my first program")
+print(40-200*10+30)
+
+
+#ctrl+D means it will duplicate the code
