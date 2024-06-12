ef946d5eeb5be9ede622ad4d9efca07d8cfc1340
sprahul333 <sprahul333@gmail.com>
2024-06-12 08:48:51 +0530

    Max Of Two Numbers and Sqaures and Cubes of Numbers

---
+++ ex_10062024/MaxOfTwoNumbers.py
@@ -0,0 +1,11 @@
+a=int(input("Enter the First number:"))
+b=int(input("Enter the Second number:"))
+
+if(a>b):
+    print("A is Greater than b")
+elif(a<b):
+    print("A is less than B")
+elif(a==b):
+    print("A is equal to B")
+else:
+    print("I dont know")
---
+++ ex_10062024/SquareAndCubeOfNumber.py
@@ -0,0 +1,4 @@
+a=int(input("Enter the number:"))
+
+print("Square of the number is:",a*a)
+print("Cube of the number is:", a*a*a)
