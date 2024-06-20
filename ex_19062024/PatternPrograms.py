bbd80357364be733ac8d391cbd0b27709a468e90
spaluvai <spaluvai@viaplus.com>
2024-06-20 16:34:43 +0530

    Using Index Concept in Lists

--- ex_19062024/PatternPrograms.py
+++ ex_19062024/PatternPrograms.py
@@ -5,3 +5,19 @@ def pattern(numberoflines):
         print("")
 
 pattern(5)
+
+print("*********************************************************************************************************")
+
+# def mystery(number):
+#     if number < 0:
+#         return 0
+#     else:
+#         # print(number)
+#         # mystery(number-1)
+#       number  + mystery(number//2) + mystery(number//4)
+
+# mystery(5)
+
+my_new_list=["Hello","World","Today","Is","A","Great","Day"]
+
+my_new_list[my_new_list.index("Today")]="Tomorrow"
