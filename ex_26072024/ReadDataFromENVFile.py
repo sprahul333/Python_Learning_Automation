79cf7ab1c45e1375872a9122e1e97507bf1867aa
SPR <sprahul333@gmail.com>
2024-08-02 23:36:59 +0530

    Updated code for Read Data from ENV File

--- ex_26072024/ReadDataFromENVFile.py
+++ ex_26072024/ReadDataFromENVFile.py
@@ -1,4 +1,4 @@
-
+import pandas
 from dotenv import find_dotenv, load_dotenv
 
 import os
@@ -7,4 +7,11 @@ import os
 def test_data_update_req():
     load_dotenv()
     url= os.getenv("URL")
+    username=os.getenv("UserName")
     print(url)
+    print(username)
+    print(type(url))
+    print(type(username))
+
+
+test_data_update_req()
